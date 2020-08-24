721a8d1bdce753a69a2fafa983b594fa7fbe1145
Allen Grant <silvergrant308@gmail.com>
2020-08-24 21:00:56 +1000

    This is my first use of the GitHub repository.

---
+++ Productivity_Tools 1b install_git.R
@@ -0,0 +1,24 @@
+# Installing Git on computers
+
+# Install on Windows
+# Download Git bash from https://git-scm.com/ External link
+
+# When asked to choose the default editor for Git, we 
+# recommend choosing nano if you do not already know VIM.
+
+# The "git and optional Unix tools from Windows" option 
+# will allow you to learn Unix from Rstudio, however, it 
+# might interfere with the Windows command line.
+
+# Finally, change the Rstudio preference so that you 
+# are using Git bash as the terminal (only for Windows user).
+
+# Install on Mac
+# Open the terminal, either from the utility folder or 
+# using Cmd+space, and check if you already have Git 
+# installed by typing git --version in the command line.
+
+# If you already have Git installed, you will be shown
+# the version number after executing the above. If you 
+# do not have Git installed already, you will be prompted
+# to do so.
